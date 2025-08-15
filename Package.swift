df22c0512b8c1c6dc486846056d1b36b894ac028
who-biz <37732338+who-biz@users.noreply.github.com>
2025-08-15 19:35:07 -0400

    Amend package names accordingly

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.19.1"),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.14.1"),
-        .package(url: "https://github.com/who-biz/zcash-light-client-ffi", branch: "verus")
+        .package(url: "https://github.com/who-biz/verus-light-client-ffi", branch: "verus")
     ],
     targets: [
         .target(
@@ -24,7 +24,7 @@ let package = Package(
             dependencies: [
                 .product(name: "SQLite", package: "SQLite.swift"),
                 .product(name: "GRPC", package: "grpc-swift"),
-                .product(name: "libzcashlc", package: "zcash-light-client-ffi")
+                .product(name: "libzcashlc", package: "verus-light-client-ffi")
             ],
             exclude: [
                 "Modules/Service/GRPC/ProtoBuf/proto/compact_formats.proto",
