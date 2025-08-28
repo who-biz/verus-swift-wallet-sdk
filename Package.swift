94a44c456dee0509cd7c2059c880c91a759c80c6
who-biz <37732338+who-biz@users.noreply.github.com>
2025-08-28 13:10:34 -0400

    Change repo of 'verus-light-client-ffi' to shallow clone

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.19.1"),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.14.1"),
-        .package(url: "https://github.com/who-biz/verus-light-client-ffi", branch: "verus")
+        .package(url: "https://github.com/who-biz/verus-lightclient-ffi", branch: "master")
     ],
     targets: [
         .target(
