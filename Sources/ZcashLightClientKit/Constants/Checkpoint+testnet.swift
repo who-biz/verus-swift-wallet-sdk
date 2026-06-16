9d6e050eb3e1906e3e51499f965fc19a17c35c6e
who-biz <37732338+who-biz@users.noreply.github.com>
2026-06-16 19:10:11 -0400

    Fix minimum checkpoint constants

--- Sources/ZcashLightClientKit/Constants/Checkpoint+testnet.swift
+++ Sources/ZcashLightClientKit/Constants/Checkpoint+testnet.swift
@@ -12,9 +12,9 @@ extension Checkpoint {
         height: 1,
         hash: "000004c52f3047ccc50d71a6b2a9a035fafd2605cc5b49c2c1d07a202a7d3b33",
         time: 1712527210,
-        saplingTree: "000000",
-        orchardTree: nil
+        tree: "000000",
+        //orchardTree: nil
     )
     
-    static let testnetCheckpointDirectory = Bundle.module.bundleURL.appendingPathComponent("checkpoints/testnet/")
+    static let testnetCheckpointDirectory = Bundle.module.bundleURL.appendingPathComponent("checkpoints/vrsctest/")
 }
